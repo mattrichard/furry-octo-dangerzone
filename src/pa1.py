079067b67f18f55278b1bc1f7ae3e35919845221
mattrichard <matthew.w.richard@gmail.com>
2013-02-19 23:15:30 -0700

    Fixed rank calculation.

--- src/pa1.py
+++ src/pa1.py
@@ -6,27 +6,38 @@ import time
 
 
 def open_file( file_name, mode ):
+    """Opens a file in the given mode, catching any errors that may occur.
+
+    Given a path to a file and mode, this will attempt to open the file
+    in that mode. If an error occurs while openning the file, a description
+    of the error will be printed. If the file was successfully open, the file
+    handle will be returned; otherwise, None will be returned.
+    """
     f = None
     try:
         f = open( file_name, mode )
     except IOError:
         print( "Failed to open file: {0}".format( file_name ) )
+    except:
+        print( "Unknown error occured while opening file: {0}".format(
+            file_name) )
     return f
 
 
-def write_file_header_info( f, file_name, total_words, distinct_words ):
+def write_file_header_info( f, input_file_name, total_words, distinct_words ):
     f.write( "Zipf's Law: rank * freq = const\n" )
     f.write( "-------------------------------\n" )
-    f.write( "File:           {0}\n".format( file_name ) )
+    f.write( "File:           {0}\n".format( input_file_name ) )
     f.write( "Total words:    {0}\n".format( total_words ) )
     f.write( "Distinct words: {0}\n".format( distinct_words ) )
-    f.write( "\n" )
 
 
 def create_wrd_file( base_file_name, word_count, distinct_words, freq_dict ):
+    # Open the output file with .wrd extension and check for failure
     fout = open_file( base_file_name + '.wrd', 'w' )
     if fout == None:
         return False
+
     write_file_header_info( fout, base_file_name + '.txt', word_count, distinct_words )
 
     for freq in sorted( list( freq_dict.keys( ) ), reverse=True ):
@@ -45,17 +56,24 @@ def create_wrd_file( base_file_name, word_count, distinct_words, freq_dict ):
 
 
 def create_csv_file( base_file_name, word_count, distinct_words, freq_dict ):
+    # Open the output file with .csv extension and check for failure
     fout = open_file( base_file_name + '.csv', 'w' )
     if fout == None:
         return False
+
     write_file_header_info( fout, base_file_name + '.txt', word_count, distinct_words )
 
+    fout.write( "\n" )
     fout.write( "rank,      freq,       r*f\n" )
 
-    rank = 0
+    words = 1
     for freq in sorted( freq_dict.keys( ), reverse=True ):
-        rank += len( freq_dict[freq] )
+        count = sum( x for x in range( words, words + len( freq_dict[freq] ) ) )
+        rank = count / len( freq_dict[freq] )
+
         fout.write( "{0:>4},{1:>10},{2:>10}\n".format( rank, freq, rank * freq ) )
+
+        words += len( freq_dict[freq] )
 
     fout.close( )
     return True
@@ -65,25 +83,29 @@ def main( argv ):
     words_dict = dict( )
     freq_dict = dict( )
     word_count = 0
-    p = re.compile( "[^A-Za-z]*[^A-Za-z']+[^A-Za-z]*" )
+    p = re.compile( "[a-z]+[a-z']*[a-z]*" )
 
+    # Verify the user provided a file as an argument.
+    # If they didn't, prompt for a file name.
     if len( argv ) < 2:
         file_name = input( "Enter a file name: " )
     else:
         file_name = argv[1]
-
-    pat = "[^A-Za-z]*[^A-Za-z']+[^A-Za-z]*"
-    p = re.compile( pat )
 
     # Open input file and check for error
     fin = open_file( file_name, 'r' )
     if fin == None:
         return False
 
+    # Read the input file line-by-line, parse words from each line,
+    # and put the word into the words dictionary.
     for line in fin:
-        for word in p.split( line ):
+        for word in p.findall( line.lower( ) ):
+            # The regular expression can return words that have a ' at the
+            # end of the word, so strip any single quotes from the end of
+            # the word.
+            word = word.strip( "'" )
             if word != '':
-                word = word.lower( )
                 if word in words_dict:
                     words_dict[word] += 1
                 else:
@@ -99,14 +121,18 @@ def main( argv ):
         else:
             freq_dict[freq] = [word]
 
-    base_file_name = file_name.rsplit( '.', 1 )[0]
+    # Remove any directories given in the path to the file
+    base_file_name = file_name.rsplit( '/', 1 )[-1]
+
+    # remove file extension
+    base_file_name = base_file_name.rsplit( '.', 1 )[0]
 
     if not create_wrd_file( base_file_name, word_count, len( words_dict ), freq_dict ):
         print( "Failed to create wrd file" )
         return False
 
     if not create_csv_file( base_file_name, word_count, len( words_dict ), freq_dict ):
-        print( "Failed to create scv file" )
+        print( "Failed to create csv file" )
         return False
 
     return True
@@ -116,5 +142,5 @@ if __name__ == '__main__':
     start_time = time.time( )
     if not main( sys.argv ):
         sys.exit( 1 )
-    end_time = 100.0 * ( time.time( ) - start_time )
+    end_time = 1000.0 * ( time.time( ) - start_time )
     print( "Elapsed time = {0} msec".format( end_time ) )
