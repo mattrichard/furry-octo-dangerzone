139037cdddd2647cda0e23ff12017f6124fa1772
mattrichard <matthew.w.richard@gmail.com>
2013-02-20 17:49:39 -0700

    Documented a few functions.

--- src/pa1.py
+++ src/pa1.py
@@ -25,11 +25,31 @@ def open_file( file_name, mode ):
 
 
 def write_file_header_info( f, input_file_name, total_words, distinct_words ):
-    f.write( "Zipf's Law: rank * freq = const\n" )
-    f.write( "-------------------------------\n" )
-    f.write( "File:           {0}\n".format( input_file_name ) )
-    f.write( "Total words:    {0}\n".format( total_words ) )
-    f.write( "Distinct words: {0}\n".format( distinct_words ) )
+    """Writes file header info related to Zipf's Law to the given file.
+
+    Given an output file handle, an input file name, the total number of words
+    in the input file, and the total number of distinct words in the file,
+    this function will format and write the given information to the output
+    file. This function is used with the create_wrd_file and create_csv_file
+    functions, as both these file have the same file header info. If any
+    errors occur while writing to the output file, an error will be printed
+    and the function will return False. Otherwise, if no errors occured, the
+    function will return True.
+    """
+    result = False
+    try:
+        f.write( "Zipf's Law: rank * freq = const\n" )
+        f.write( "-------------------------------\n" )
+        f.write( "File:           {0}\n".format( input_file_name ) )
+        f.write( "Total words:    {0}\n".format( total_words ) )
+        f.write( "Distinct words: {0}\n".format( distinct_words ) )
+        result = True
+    except AttributeError:
+        print( "Error while writing file header info. " \
+            "The given file handle is not valid, or is opened for read only." )
+    except:
+        print( "Unknown error occured while writing file header info." )
+    return result
 
 
 def create_wrd_file( base_file_name, word_count, distinct_words, freq_dict ):
@@ -38,11 +58,17 @@ def create_wrd_file( base_file_name, word_count, distinct_words, freq_dict ):
     if fout == None:
         return False
 
-    write_file_header_info( fout, base_file_name + '.txt', word_count, distinct_words )
+    # Write header info the to wrd file, and verify it was successful
+    if not write_file_header_info( fout, base_file_name + '.txt', word_count,
+        distinct_words ):
+        fout.close( )
+        return False
 
     for freq in sorted( list( freq_dict.keys( ) ), reverse=True ):
         fout.write( "\n" )
-        fout.write( "Words occurring {0}:\n".format( "once" if freq == 1 else "{0} times".format( freq ) ) )
+        fout.write( "Words occurring {0}:\n".format(
+            "once" if freq == 1 else "{0} times".format( freq ) ) )
+
         count = 1
         for word in sorted( freq_dict[freq] ):
             fout.write( '{0:<16}'.format( word ) )
@@ -61,16 +87,22 @@ def create_csv_file( base_file_name, word_count, distinct_words, freq_dict ):
     if fout == None:
         return False
 
-    write_file_header_info( fout, base_file_name + '.txt', word_count, distinct_words )
+    # Write header info the to csv file, and verify it was successful
+    if not write_file_header_info( fout, base_file_name + '.txt', word_count,
+        distinct_words ):
+        fout.close( )
+        return false
 
     fout.write( "\n" )
-    fout.write( "rank,      freq,       r*f\n" )
+    fout.write( "{0:>4},{1:>10},{2:10}\n".format( "rank", "freq", "r*f" ) )
 
     words = 1
     for freq in sorted( freq_dict.keys( ), reverse=True ):
+        # Calculate rank
         count = sum( x for x in range( words, words + len( freq_dict[freq] ) ) )
         rank = count / len( freq_dict[freq] )
 
+        # Write rank, frequency, and rank * frequency to the csv file, formatted
         fout.write( "{0:>4},{1:>10},{2:>10}\n".format( rank, freq, rank * freq ) )
 
         words += len( freq_dict[freq] )
@@ -114,6 +146,9 @@ def main( argv ):
 
     fin.close( )
 
+    # Using the words dictionary, create a dictionary based of off the
+    # frequencies of words in the words dict. This frequency dictionary
+    # stores the words associated with each frequency in a list.
     for word in words_dict:
         freq = words_dict[word]
         if freq in freq_dict:
@@ -140,7 +175,9 @@ def main( argv ):
 
 if __name__ == '__main__':
     start_time = time.time( )
+
     if not main( sys.argv ):
         sys.exit( 1 )
+
     end_time = 1000.0 * ( time.time( ) - start_time )
     print( "Elapsed time = {0} msec".format( end_time ) )
